cf10d24c35d7b6b7682afba3d3af202eb25ea190
vintvgx <69157141+vintvgx@users.noreply.github.com>
2022-01-31 01:39:23 -0500

    Updates

---
+++ precache-manifest.3c3e6a5025af0385c78431c8195b4c63.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997cf8776d057383e52253d9e0e4da7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58cd963d01bdbd549b10",
+    "url": "/static/css/main.611c29aa.chunk.css"
+  },
+  {
+    "revision": "eea2f7e0fdcdff6a5bea",
+    "url": "/static/js/2.c47bd007.chunk.js"
+  },
+  {
+    "revision": "80cd043360d9e9c6e3297a237ff5b2a9",
+    "url": "/static/js/2.c47bd007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cd963d01bdbd549b10",
+    "url": "/static/js/main.15575e57.chunk.js"
+  },
+  {
+    "revision": "b81dc53b53e1731ca50d",
+    "url": "/static/js/runtime-main.f87e9630.js"
+  },
+  {
+    "revision": "305762fc782eb5d7d6ec5bd1bfc87335",
+    "url": "/static/media/Adobe.305762fc.png"
+  },
+  {
+    "revision": "1231be6c41f1c8e7a70755ca7db5998f",
+    "url": "/static/media/CSS.1231be6c.png"
+  },
+  {
+    "revision": "aaa5d5dc4bad2276bf41780308a2ef26",
+    "url": "/static/media/Iphone13Launch.aaa5d5dc.png"
+  },
+  {
+    "revision": "d3304b833b96ca9b6b4d4f12d07d7176",
+    "url": "/static/media/JS.d3304b83.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/static/media/Node.4c6c0929.png"
+  },
+  {
+    "revision": "2d2b0d56aeefb1b0f3025195cb2ff7b6",
+    "url": "/static/media/React.2d2b0d56.png"
+  },
+  {
+    "revision": "427ffceaf88e467c9916ef614968adfe",
+    "url": "/static/media/banditTech.427ffcea.png"
+  },
+  {
+    "revision": "28a89d2875debb3f93da467413a72667",
+    "url": "/static/media/comptia.28a89d28.png"
+  },
+  {
+    "revision": "ccc745cd97f630252feef4d1caa0eead",
+    "url": "/static/media/profile.ccc745cd.jpg"
+  },
+  {
+    "revision": "553d31f443d6075b3ac930cdb6c425cd",
+    "url": "/static/media/resume.553d31f4.pdf"
+  }
+]);
